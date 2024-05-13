ae8ae4de21b4c3d43f1a258f076faf7fdd83153e
july1012 <113192987+july1012@users.noreply.github.com>
2024-05-13 13:13:07 -0400

    Update Signin.vue

---
+++ my-app/src/views/Signin.vue
@@ -0,0 +1,8 @@
+FormData
+<input>Username:</input>
+Input: Password
+Submit <Button></Button>
+
+if(input.usenrame = supabaseusername && supabasepassword = input.password){
+    routerlink to homeview
+}
