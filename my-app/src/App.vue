e55377d42cc127161c05c195fc1484b0cb03ac2e
jolenexiong <144476558+jolenexiong@users.noreply.github.com>
2024-05-31 13:03:00 -0400

    Update App.vue

--- my-app/src/App.vue
+++ my-app/src/App.vue
@@ -6,15 +6,7 @@ import { supabase } from './supabase'
 
 const session = ref()
 
-onMounted(() => {
-  supabase.auth.getSession().then(({ data }) => {
-    session.value = data.session
-  })
 
-  supabase.auth.onAuthStateChange((_, _session) => {
-    session.value = _session
-  })
-})
 </script>
 
 <template>
